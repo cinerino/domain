5dcfc2cd6888ed3eca2cb956c0bca69a786908c4
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-09-21 06:48:30 +0000

    # CINERINO-2 ### Changed - 注文取引レポートを調整。

--- src/service/report/transaction.ts
+++ src/service/report/transaction.ts
@@ -4,6 +4,7 @@
 // import * as createDebug from 'debug';
 import * as json2csv from 'json2csv';
 import * as moment from 'moment';
+import { Readable } from 'stream';
 import * as util from 'util';
 
 import * as factory from '../../factory';
@@ -75,7 +76,7 @@ export function download(params: {
     conditions: factory.transaction.ISearchConditions<factory.transactionType.PlaceOrder>;
     format: factory.encodingFormat.Text;
 }) {
-    return async (repos: { transaction: TransactionRepo }): Promise<json2csv.Transform<any>> => {
+    return async (repos: { transaction: TransactionRepo }): Promise<Readable> => {
         // const transactionCount = await repos.transaction.count<factory.transactionType.PlaceOrder>(params.conditions);
         // debug('transactionCount:', transactionCount);
         // if (transactionCount >= 0) {
@@ -87,88 +88,88 @@ export function download(params: {
                 transaction: doc.toObject()
             }));
         });
+        let processor: Readable;
 
         switch (params.format) {
             case factory.encodingFormat.Text.csv:
-                return new Promise<json2csv.Transform<any>>((resolve) => {
-                    const fields: json2csv.json2csv.FieldInfo<any>[] = [
-                        { label: '取引ID', default: '', value: 'id' },
-                        { label: '取引ステータス', default: '', value: 'status' },
-                        { label: '取引開始日時', default: '', value: 'startDate' },
-                        { label: '取引終了日時', default: '', value: 'endDate' },
-                        { label: '購入者タイプ', default: '', value: 'customer.typeOf' },
-                        { label: '購入者ID', default: '', value: 'customer.id' },
-                        { label: '購入者お名前', default: '', value: 'customer.name' },
-                        { label: '購入者メールアドレス', default: '', value: 'customer.email' },
-                        { label: '購入者電話番号', default: '', value: 'customer.telephone' },
-                        { label: '購入者会員ID', default: '', value: 'customer.memberOf.membershipNumber' },
-                        { label: '購入者トークン発行者', default: '', value: 'customer.tokenIssuer' },
-                        { label: '購入者クライアント', default: '', value: 'customer.clientId' },
-                        { label: '販売者タイプ', default: '', value: 'seller.typeOf' },
-                        { label: '販売者ID', default: '', value: 'seller.id' },
-                        { label: '販売者名', default: '', value: 'seller.name' },
-                        { label: '販売者URL', default: '', value: 'seller.url' },
-                        { label: '注文番号', default: '', value: 'orderNumber' },
-                        { label: '確認番号', default: '', value: 'confirmationNumber' },
-                        { label: '注文アイテムタイプ', default: '', value: 'items.typeOf' },
-                        { label: '注文アイテムチケット金額', default: '', value: 'items.totalPrice' },
-                        { label: '注文アイテム名', default: '', value: 'items.name' },
-                        { label: '注文アイテム数', default: '', value: 'items.numItems' },
-                        { label: '注文アイテムイベントタイプ', default: '', value: 'items.event.typeOf' },
-                        { label: '注文アイテムイベントID', default: '', value: 'items.event.id' },
-                        { label: '注文アイテムイベント名', default: '', value: 'items.event.name' },
-                        { label: '注文アイテムイベント開始日時', default: '', value: 'items.event.startDate' },
-                        { label: '注文アイテムイベント終了日時', default: '', value: 'items.event.endDate' },
-                        { label: '注文アイテムイベント場所', default: '', value: 'items.event.location' },
-                        { label: '注文アイテム親イベント場所枝番号', default: '', value: 'items.event.superEventLocationBranchCode' },
-                        { label: '注文アイテム親イベント場所', default: '', value: 'items.event.superEventLocation' },
-                        { label: '注文金額', default: '', value: 'price' },
-                        { label: '決済方法1', default: '', value: 'paymentMethodType.0' },
-                        { label: '決済ID1', default: '', value: 'paymentMethodId.0' },
-                        { label: '決済方法2', default: '', value: 'paymentMethodType.1' },
-                        { label: '決済ID2', default: '', value: 'paymentMethodId.1' },
-                        { label: '決済方法3', default: '', value: 'paymentMethodType.2' },
-                        { label: '決済ID3', default: '', value: 'paymentMethodId.2' },
-                        { label: '決済方法4', default: '', value: 'paymentMethodType.3' },
-                        { label: '決済ID4', default: '', value: 'paymentMethodId.3' },
-                        { label: '割引1', default: '', value: 'discounts.0' },
-                        { label: '割引コード1', default: '', value: 'discountCodes.0' },
-                        { label: '割引金額1', default: '', value: 'discountPrices.0' },
-                        { label: '割引2', default: '', value: 'discounts.1' },
-                        { label: '割引コード2', default: '', value: 'discountCodes.1' },
-                        { label: '割引金額2', default: '', value: 'discountPrices.1' },
-                        { label: '割引3', default: '', value: 'discounts.2' },
-                        { label: '割引コード3', default: '', value: 'discountCodes.2' },
-                        { label: '割引金額3', default: '', value: 'discountPrices.2' },
-                        { label: '割引4', default: '', value: 'discounts.3' },
-                        { label: '割引コード4', default: '', value: 'discountCodes.3' },
-                        { label: '割引金額4', default: '', value: 'discountPrices.3' }
-                        // { label: '注文状況', default: '', value: 'orderStatus' },
-                        // { label: '予約チケットステータス', default: '', value: 'items.reservationStatus' },
-                        // { label: '予約チケットチェックイン数', default: '', value: 'items.numCheckInActions' }
-                    ];
+                const fields: json2csv.json2csv.FieldInfo<any>[] = [
+                    { label: '取引ID', default: '', value: 'id' },
+                    { label: '取引ステータス', default: '', value: 'status' },
+                    { label: '取引開始日時', default: '', value: 'startDate' },
+                    { label: '取引終了日時', default: '', value: 'endDate' },
+                    { label: '購入者タイプ', default: '', value: 'customer.typeOf' },
+                    { label: '購入者ID', default: '', value: 'customer.id' },
+                    { label: '購入者お名前', default: '', value: 'customer.name' },
+                    { label: '購入者メールアドレス', default: '', value: 'customer.email' },
+                    { label: '購入者電話番号', default: '', value: 'customer.telephone' },
+                    { label: '購入者会員ID', default: '', value: 'customer.memberOf.membershipNumber' },
+                    { label: '購入者トークン発行者', default: '', value: 'customer.tokenIssuer' },
+                    { label: '購入者クライアント', default: '', value: 'customer.clientId' },
+                    { label: '販売者タイプ', default: '', value: 'seller.typeOf' },
+                    { label: '販売者ID', default: '', value: 'seller.id' },
+                    { label: '販売者名', default: '', value: 'seller.name' },
+                    { label: '販売者URL', default: '', value: 'seller.url' },
+                    { label: '注文番号', default: '', value: 'orderNumber' },
+                    { label: '確認番号', default: '', value: 'confirmationNumber' },
+                    { label: '注文アイテムタイプ', default: '', value: 'items.typeOf' },
+                    { label: '注文アイテムチケット金額', default: '', value: 'items.totalPrice' },
+                    { label: '注文アイテム名', default: '', value: 'items.name' },
+                    { label: '注文アイテム数', default: '', value: 'items.numItems' },
+                    { label: '注文アイテムイベントタイプ', default: '', value: 'items.event.typeOf' },
+                    { label: '注文アイテムイベントID', default: '', value: 'items.event.id' },
+                    { label: '注文アイテムイベント名', default: '', value: 'items.event.name' },
+                    { label: '注文アイテムイベント開始日時', default: '', value: 'items.event.startDate' },
+                    { label: '注文アイテムイベント終了日時', default: '', value: 'items.event.endDate' },
+                    { label: '注文アイテムイベント場所', default: '', value: 'items.event.location' },
+                    { label: '注文アイテム親イベント場所枝番号', default: '', value: 'items.event.superEventLocationBranchCode' },
+                    { label: '注文アイテム親イベント場所', default: '', value: 'items.event.superEventLocation' },
+                    { label: '注文金額', default: '', value: 'price' },
+                    { label: '決済方法1', default: '', value: 'paymentMethodType.0' },
+                    { label: '決済ID1', default: '', value: 'paymentMethodId.0' },
+                    { label: '決済方法2', default: '', value: 'paymentMethodType.1' },
+                    { label: '決済ID2', default: '', value: 'paymentMethodId.1' },
+                    { label: '決済方法3', default: '', value: 'paymentMethodType.2' },
+                    { label: '決済ID3', default: '', value: 'paymentMethodId.2' },
+                    { label: '決済方法4', default: '', value: 'paymentMethodType.3' },
+                    { label: '決済ID4', default: '', value: 'paymentMethodId.3' },
+                    { label: '割引1', default: '', value: 'discounts.0' },
+                    { label: '割引コード1', default: '', value: 'discountCodes.0' },
+                    { label: '割引金額1', default: '', value: 'discountPrices.0' },
+                    { label: '割引2', default: '', value: 'discounts.1' },
+                    { label: '割引コード2', default: '', value: 'discountCodes.1' },
+                    { label: '割引金額2', default: '', value: 'discountPrices.1' },
+                    { label: '割引3', default: '', value: 'discounts.2' },
+                    { label: '割引コード3', default: '', value: 'discountCodes.2' },
+                    { label: '割引金額3', default: '', value: 'discountPrices.2' },
+                    { label: '割引4', default: '', value: 'discounts.3' },
+                    { label: '割引コード4', default: '', value: 'discountCodes.3' },
+                    { label: '割引金額4', default: '', value: 'discountPrices.3' }
+                    // { label: '注文状況', default: '', value: 'orderStatus' },
+                    // { label: '予約チケットステータス', default: '', value: 'items.reservationStatus' },
+                    // { label: '予約チケットチェックイン数', default: '', value: 'items.numCheckInActions' }
+                ];
 
-                    const opts = {
-                        fields: fields,
-                        delimiter: ',',
-                        eol: '\n',
-                        // flatten: true,
-                        // preserveNewLinesInValues: true,
-                        unwind: 'items'
-                    };
-                    // const json2csvParser = new json2csv.Parser(opts);
-                    const transformOpts = {
-                        highWaterMark: 16384,
-                        encoding: 'utf-8'
-                    };
-                    const transform = new json2csv.Transform(opts, transformOpts);
-                    const processor = inputStream.pipe(transform);
-                    resolve(processor);
-                });
-
+                const opts = {
+                    fields: fields,
+                    delimiter: ',',
+                    eol: '\n',
+                    // flatten: true,
+                    // preserveNewLinesInValues: true,
+                    unwind: 'items'
+                };
+                // const json2csvParser = new json2csv.Parser(opts);
+                const transformOpts = {
+                    highWaterMark: 16384,
+                    encoding: 'utf-8'
+                };
+                const transform = new json2csv.Transform(opts, transformOpts);
+                processor = inputStream.pipe(transform);
+                break;
             default:
-                throw new factory.errors.NotImplemented('Unimplemented format');
+                throw new factory.errors.NotImplemented(`Unimplemented format: ${params.format}`);
         }
+
+        return processor;
     };
 }
 // tslint:disable-next-line:no-single-line-block-comment
