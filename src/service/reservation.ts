0ea8935fdf15d9b6a6cb2ff6b063531ab07b64bc
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-12-03 06:53:44 +0000

    # CINERINO-30 ### Changed - 予約確定タスクを追加

--- src/service/reservation.ts
+++ src/service/reservation.ts
@@ -1,11 +1,12 @@
 /**
  * 予約サービス
- * 予約の保管先はChevreサービスです。
+ * 予約の保管先はChevreです
  */
 import * as chevre from '../chevre';
 import * as factory from '../factory';
 
 import { handleChevreError } from '../errorHandler';
+import { MongoRepository as ActionRepo } from '../repo/action';
 import { MongoRepository as OwnershipInfoRepo } from '../repo/ownershipInfo';
 
 type IOwnershipInfoWithDetail =
@@ -14,6 +15,41 @@ export type ISearchScreeningEventReservationsOperation<T> = (repos: {
     ownershipInfo: OwnershipInfoRepo;
     reservationService: chevre.service.Reservation;
 }) => Promise<T>;
+
+/**
+ * 予約を確定する
+ */
+export function confirmReservation(params: factory.action.interact.confirm.reservation.IAttributes) {
+    return async (repos: {
+        action: ActionRepo;
+        reserveService: chevre.service.transaction.Reserve;
+    }) => {
+        // アクション開始
+        const confirmActionAttributes = params;
+        const action = await repos.action.start(confirmActionAttributes);
+
+        try {
+            // 座席予約確定
+            await repos.reserveService.confirm(confirmActionAttributes.object);
+        } catch (error) {
+            // actionにエラー結果を追加
+            try {
+                const actionError = { ...error, message: error.message, name: error.name };
+                await repos.action.giveUp({ typeOf: confirmActionAttributes.typeOf, id: action.id, error: actionError });
+            } catch (__) {
+                // 失敗したら仕方ない
+            }
+
+            throw error;
+        }
+
+        // アクション完了
+        const result: factory.action.interact.confirm.reservation.IResult = {
+        };
+        await repos.action.complete({ typeOf: confirmActionAttributes.typeOf, id: action.id, result: result });
+    };
+}
+
 /**
  * 上映イベント予約検索
  */
