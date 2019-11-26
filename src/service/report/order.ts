0d2aa1eab56984e92f799bddf16a2f2a3f1f5e12
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-11-26 00:28:07 +0000

    ### Changed - 注文CSVレポートを拡張

--- src/service/report/order.ts
+++ src/service/report/order.ts
@@ -16,46 +16,63 @@ export type ITaskAndTransactionOperation<T> = (repos: {
     task: TaskRepo;
 }) => Promise<T>;
 
+export interface ISeller {
+    typeOf: string;
+    id: string;
+    name: string;
+    url: string;
+}
+
+export interface ICustomer {
+    typeOf: string;
+    id: string;
+    name: string;
+    email: string;
+    telephone: string;
+    memberOf?: {
+        membershipNumber?: string;
+    };
+    clientId: string;
+    tokenIssuer: string;
+}
+
+export interface IItem {
+    typeOf: string;
+    name: string;
+    numItems: number;
+    id: string;
+    event: {
+        typeOf: string;
+        id: string;
+        name: string;
+        startDate: string;
+        endDate: string;
+        location: string;
+        superEventLocationBranchCode: string;
+        superEventLocation: string;
+    };
+}
+
+export interface IAcceptedOffer {
+    typeOf: string;
+    name: string;
+    id: string;
+    unitPriceSpecification: {
+        price: String;
+        priceCurrency: string;
+    };
+    itemOffered: IItem;
+}
+
 /**
  * 注文レポートインターフェース
  */
 export interface IOrderReport {
     orderStatus: string;
     orderDate: string;
-    seller: {
-        typeOf: string;
-        id: string;
-        name: string;
-        url: string;
-    };
-    customer: {
-        typeOf: string;
-        id: string;
-        name: string;
-        email: string;
-        telephone: string;
-        memberOf?: {
-            membershipNumber?: string;
-        };
-        clientId: string;
-        tokenIssuer: string;
-    };
-    items: {
-        typeOf: string;
-        name: string;
-        numItems: number;
-        id: string;
-        event: {
-            typeOf: string;
-            id: string;
-            name: string;
-            startDate: string;
-            endDate: string;
-            location: string;
-            superEventLocationBranchCode: string;
-            superEventLocation: string;
-        };
-    }[];
+    seller: ISeller;
+    customer: ICustomer;
+    acceptedOffers: IAcceptedOffer[];
     orderNumber: string;
     confirmationNumber: string;
     price: string;
@@ -95,41 +112,45 @@ export function stream(params: {
                 const fields: json2csv.default.FieldInfo<any>[] = [
                     { label: '注文ステータス', default: '', value: 'orderStatus' },
                     { label: '注文日時', default: '', value: 'orderDate' },
+                    { label: '注文番号', default: '', value: 'orderNumber' },
+                    { label: '確認番号', default: '', value: 'confirmationNumber' },
+                    { label: '金額', default: '', value: 'price' },
                     { label: '購入者タイプ', default: '', value: 'customer.typeOf' },
                     { label: '購入者ID', default: '', value: 'customer.id' },
-                    { label: '購入者お名前', default: '', value: 'customer.name' },
+                    { label: '購入者名称', default: '', value: 'customer.name' },
                     { label: '購入者メールアドレス', default: '', value: 'customer.email' },
                     { label: '購入者電話番号', default: '', value: 'customer.telephone' },
-                    { label: '購入者会員ID', default: '', value: 'customer.memberOf.membershipNumber' },
+                    { label: '購入者会員番号', default: '', value: 'customer.memberOf.membershipNumber' },
                     { label: '購入者トークン発行者', default: '', value: 'customer.tokenIssuer' },
                     { label: '購入者クライアント', default: '', value: 'customer.clientId' },
                     { label: '販売者タイプ', default: '', value: 'seller.typeOf' },
                     { label: '販売者ID', default: '', value: 'seller.id' },
-                    { label: '販売者名', default: '', value: 'seller.name' },
+                    { label: '販売者名称', default: '', value: 'seller.name' },
                     { label: '販売者URL', default: '', value: 'seller.url' },
-                    { label: '注文番号', default: '', value: 'orderNumber' },
-                    { label: '確認番号', default: '', value: 'confirmationNumber' },
-                    { label: '注文アイテムタイプ', default: '', value: 'items.typeOf' },
-                    // { label: '注文アイテムチケット金額', default: '', value: 'items.totalPrice' },
-                    { label: '注文アイテムID', default: '', value: 'items.id' },
-                    { label: '注文アイテム名', default: '', value: 'items.name' },
-                    { label: '注文アイテム数', default: '', value: 'items.numItems' },
-                    { label: '注文アイテムイベントタイプ', default: '', value: 'items.event.typeOf' },
-                    { label: '注文アイテムイベントID', default: '', value: 'items.event.id' },
-                    { label: '注文アイテムイベント名', default: '', value: 'items.event.name' },
-                    { label: '注文アイテムイベント開始日時', default: '', value: 'items.event.startDate' },
-                    { label: '注文アイテムイベント終了日時', default: '', value: 'items.event.endDate' },
-                    { label: '注文アイテムイベント場所', default: '', value: 'items.event.location' },
-                    { label: '注文アイテム親イベント場所枝番号', default: '', value: 'items.event.superEventLocationBranchCode' },
-                    { label: '注文アイテム親イベント場所', default: '', value: 'items.event.superEventLocation' },
-                    { label: '注文金額', default: '', value: 'price' },
-                    { label: '決済方法1', default: '', value: 'paymentMethodType.0' },
+                    { label: 'オファータイプ', default: '', value: 'acceptedOffers.typeOf' },
+                    { label: 'オファーID', default: '', value: 'acceptedOffers.id' },
+                    { label: 'オファー名称', default: '', value: 'acceptedOffers.name' },
+                    { label: 'オファー単価仕様価格', default: '', value: 'acceptedOffers.unitPriceSpecification.price' },
+                    { label: 'オファー単価仕様通貨', default: '', value: 'acceptedOffers.unitPriceSpecification.priceCurrency' },
+                    { label: '注文アイテムタイプ', default: '', value: 'acceptedOffers.itemOffered.typeOf' },
+                    { label: '注文アイテムID', default: '', value: 'acceptedOffers.itemOffered.id' },
+                    { label: '注文アイテム名称', default: '', value: 'acceptedOffers.itemOffered.name' },
+                    { label: '注文アイテム数', default: '', value: 'acceptedOffers.itemOffered.numItems' },
+                    { label: '注文アイテムイベントタイプ', default: '', value: 'acceptedOffers.itemOffered.event.typeOf' },
+                    { label: '注文アイテムイベントID', default: '', value: 'acceptedOffers.itemOffered.event.id' },
+                    { label: '注文アイテムイベント名称', default: '', value: 'acceptedOffers.itemOffered.event.name' },
+                    { label: '注文アイテムイベント開始日時', default: '', value: 'acceptedOffers.itemOffered.event.startDate' },
+                    { label: '注文アイテムイベント終了日時', default: '', value: 'acceptedOffers.itemOffered.event.endDate' },
+                    { label: '注文アイテムイベント場所', default: '', value: 'acceptedOffers.itemOffered.event.location' },
+                    { label: '注文アイテム親イベント場所枝番号', default: '', value: 'acceptedOffers.itemOffered.event.superEventLocationBranchCode' },
+                    { label: '注文アイテム親イベント場所', default: '', value: 'acceptedOffers.itemOffered.event.superEventLocation' },
+                    { label: '決済方法タイプ1', default: '', value: 'paymentMethodType.0' },
                     { label: '決済ID1', default: '', value: 'paymentMethodId.0' },
-                    { label: '決済方法2', default: '', value: 'paymentMethodType.1' },
+                    { label: '決済方法タイプ2', default: '', value: 'paymentMethodType.1' },
                     { label: '決済ID2', default: '', value: 'paymentMethodId.1' },
-                    { label: '決済方法3', default: '', value: 'paymentMethodType.2' },
+                    { label: '決済方法タイプ3', default: '', value: 'paymentMethodType.2' },
                     { label: '決済ID3', default: '', value: 'paymentMethodId.2' },
-                    { label: '決済方法4', default: '', value: 'paymentMethodType.3' },
+                    { label: '決済方法タイプ4', default: '', value: 'paymentMethodType.3' },
                     { label: '決済ID4', default: '', value: 'paymentMethodId.3' }
                 ];
 
@@ -139,7 +160,7 @@ export function stream(params: {
                     eol: '\n',
                     // flatten: true,
                     // preserveNewLinesInValues: true,
-                    unwind: 'items'
+                    unwind: 'acceptedOffers'
                 };
                 // const json2csvParser = new json2csv.Parser(opts);
                 const transformOpts = {
@@ -163,18 +184,56 @@ export function stream(params: {
     };
 }
 
-// tslint:disable-next-line:max-func-body-length
 export function order2report(params: {
     order: factory.order.IOrder;
 }): IOrderReport {
     const order = params.order;
     let event: factory.chevre.event.IEvent<factory.chevre.eventType.ScreeningEvent> | undefined;
-    const items = order.acceptedOffers.map(
-        // tslint:disable-next-line:cyclomatic-complexity
-        (orderItem) => {
-            const offer = orderItem.itemOffered;
-            let item = {
-                typeOf: String(offer.typeOf),
+
+    const acceptedOffers: IAcceptedOffer[] = order.acceptedOffers.map(
+        // tslint:disable-next-line:cyclomatic-complexity max-func-body-length
+        (acceptedOffer) => {
+            let unitPriceSpecification: { price: string; priceCurrency: string } | undefined;
+
+            if (acceptedOffer.priceSpecification !== undefined) {
+                switch (acceptedOffer.priceSpecification.typeOf) {
+                    case factory.chevre.priceSpecificationType.UnitPriceSpecification:
+                        const priceSpec
+                            // tslint:disable-next-line:max-line-length
+                            = <factory.chevre.priceSpecification.IPriceSpecification<factory.chevre.priceSpecificationType.UnitPriceSpecification>>
+                            acceptedOffer.priceSpecification;
+
+                        unitPriceSpecification = {
+                            price: (typeof priceSpec.price === 'number') ? String(priceSpec.price) : '',
+                            priceCurrency: String(priceSpec.priceCurrency)
+                        };
+
+                        break;
+
+                    case factory.chevre.priceSpecificationType.CompoundPriceSpecification:
+                        const compoundPriceSpec
+                            = <factory.chevre.compoundPriceSpecification.IPriceSpecification<factory.chevre.priceSpecificationType>>
+                            acceptedOffer.priceSpecification;
+
+                        const unitPriceSpec = compoundPriceSpec.priceComponent.find(
+                            (component) => component.typeOf === factory.chevre.priceSpecificationType.UnitPriceSpecification
+                        );
+                        if (unitPriceSpec !== undefined) {
+                            unitPriceSpecification = {
+                                price: (typeof unitPriceSpec.price === 'number') ? String(unitPriceSpec.price) : '',
+                                priceCurrency: String(unitPriceSpec.priceCurrency)
+                            };
+                        }
+
+                        break;
+
+                    default:
+                }
+            }
+
+            const itemOffered = acceptedOffer.itemOffered;
+            let item: IItem = {
+                typeOf: String(itemOffered.typeOf),
                 name: '',
                 numItems: 1,
                 id: '',
@@ -190,10 +249,10 @@ export function order2report(params: {
                 }
             };
 
-            switch (offer.typeOf) {
+            switch (itemOffered.typeOf) {
                 case factory.chevre.reservationType.EventReservation:
-                    event = offer.reservationFor;
-                    const ticket = offer.reservedTicket;
+                    event = itemOffered.reservationFor;
+                    const ticket = itemOffered.reservedTicket;
                     const ticketedSeat = ticket.ticketedSeat;
 
                     let name = '';
@@ -201,19 +260,19 @@ export function order2report(params: {
 
                     name = [
                         (ticketedSeat !== undefined) ? ticketedSeat.seatNumber : '',
-                        offer.reservedTicket.ticketType.name.ja
+                        itemOffered.reservedTicket.ticketType.name.ja
                     ].join(' ');
 
-                    if (offer.numSeats !== undefined) {
+                    if (itemOffered.numSeats !== undefined) {
                         // tslint:disable-next-line:max-line-length
-                        numItems = offer.numSeats;
+                        numItems = itemOffered.numSeats;
                     }
 
                     item = {
-                        typeOf: offer.typeOf,
+                        typeOf: itemOffered.typeOf,
                         name: name,
                         numItems: numItems,
-                        id: offer.id,
+                        id: itemOffered.id,
                         event: {
                             typeOf: (event !== undefined) ? event.typeOf : '',
                             id: (event !== undefined) ? event.id : '',
@@ -231,10 +290,10 @@ export function order2report(params: {
 
                 case factory.programMembership.ProgramMembershipType.ProgramMembership:
                     item = {
-                        typeOf: String(offer.typeOf),
-                        name: (typeof offer.name === 'string') ? offer.name : '',
+                        typeOf: String(itemOffered.typeOf),
+                        name: (typeof itemOffered.name === 'string') ? itemOffered.name : '',
                         numItems: 1,
-                        id: (typeof offer.id === 'string') ? offer.id : '',
+                        id: (typeof itemOffered.id === 'string') ? itemOffered.id : '',
                         event: {
                             typeOf: '',
                             id: '',
@@ -251,7 +310,18 @@ export function order2report(params: {
                 default:
             }
 
-            return item;
+            return {
+                typeOf: acceptedOffer.typeOf,
+                id: (typeof acceptedOffer.id === 'string') ? acceptedOffer.id : '',
+                name: (typeof acceptedOffer.name === 'string')
+                    ? acceptedOffer.name
+                    : (acceptedOffer.name !== undefined && acceptedOffer.name !== null) ? acceptedOffer.name.ja : '',
+                unitPriceSpecification: {
+                    price: (unitPriceSpecification !== undefined) ? unitPriceSpecification.price : '',
+                    priceCurrency: (unitPriceSpecification !== undefined) ? unitPriceSpecification.priceCurrency : ''
+                },
+                itemOffered: item
+            };
         }
     );
 
@@ -278,7 +348,7 @@ export function order2report(params: {
             clientId: (clientIdProperty !== undefined) ? clientIdProperty.value : '',
             tokenIssuer: (tokenIssuerProperty !== undefined) ? tokenIssuerProperty.value : ''
         },
-        items: items,
+        acceptedOffers: acceptedOffers,
         orderNumber: order.orderNumber,
         orderStatus: order.orderStatus,
         confirmationNumber: order.confirmationNumber.toString(),
