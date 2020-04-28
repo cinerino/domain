b68cc72bf10f4be6a5e685abc5c3d909151b7030
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-04-28 23:06:26 +0000

    ### Added - 取引確定後アクションパラメータにインセンティブ付与を追加

--- src/service/transaction/placeOrderInProgress/potentialActions/givePointAward.ts
+++ src/service/transaction/placeOrderInProgress/potentialActions/givePointAward.ts
@@ -7,49 +7,47 @@ export async function createGivePointAwardActions(params: {
 }): Promise<factory.action.transfer.give.pointAward.IAttributes[]> {
     let actions: factory.action.transfer.give.pointAward.IAttributes[] = [];
 
-    // 取引agentに所有メンバーシップがあれば、そちらを元にインセンティブ付与アクションを作成
+    // インセンティブ付与アクションの指定があればそちらを反映
     const givePointAwardParams = params.potentialActions?.order?.potentialActions?.givePointAward;
     if (Array.isArray(givePointAwardParams)) {
-        if (params.order.price > 0) {
-            // メンバーシップごとに、会員プログラムの特典を確認してインセンティブ付与
-            givePointAwardParams.forEach((givePointAwardParam) => {
-                const amount = givePointAwardParam.object?.amount;
-                const accountNumber = givePointAwardParam.object?.toLocation?.accountNumber;
-                const accountType = givePointAwardParam.object?.toLocation?.accountType;
-                const description = givePointAwardParam.object?.description;
+        // メンバーシップごとに、会員プログラムの特典を確認してインセンティブ付与
+        givePointAwardParams.forEach((givePointAwardParam) => {
+            const amount = givePointAwardParam.object?.amount;
+            const accountNumber = givePointAwardParam.object?.toLocation?.accountNumber;
+            const accountType = givePointAwardParam.object?.toLocation?.accountType;
+            const description = givePointAwardParam.object?.description;
 
-                if (typeof amount === 'number'
-                    && typeof accountNumber === 'string'
-                    && typeof accountType === 'string') {
-                    actions.push({
-                        project: params.transaction.project,
-                        typeOf: <factory.actionType.GiveAction>factory.actionType.GiveAction,
-                        agent: params.transaction.seller,
-                        recipient: params.transaction.agent,
-                        object: {
-                            typeOf: factory.action.transfer.give.pointAward.ObjectType.PointAward,
-                            amount: amount,
-                            toLocation: {
-                                accountNumber: accountNumber,
-                                accountType: accountType
-                            },
-                            description: (typeof description === 'string') ? description : ''
+            if (typeof amount === 'number'
+                && typeof accountNumber === 'string'
+                && typeof accountType === 'string') {
+                actions.push({
+                    project: params.transaction.project,
+                    typeOf: <factory.actionType.GiveAction>factory.actionType.GiveAction,
+                    agent: params.transaction.seller,
+                    recipient: params.transaction.agent,
+                    object: {
+                        typeOf: factory.action.transfer.give.pointAward.ObjectType.PointAward,
+                        amount: amount,
+                        toLocation: {
+                            accountNumber: accountNumber,
+                            accountType: accountType
                         },
-                        purpose: {
-                            project: params.order.project,
-                            typeOf: params.order.typeOf,
-                            seller: params.order.seller,
-                            customer: params.order.customer,
-                            confirmationNumber: params.order.confirmationNumber,
-                            orderNumber: params.order.orderNumber,
-                            price: params.order.price,
-                            priceCurrency: params.order.priceCurrency,
-                            orderDate: params.order.orderDate
-                        }
-                    });
-                }
-            });
-        }
+                        description: (typeof description === 'string') ? description : ''
+                    },
+                    purpose: {
+                        project: params.order.project,
+                        typeOf: params.order.typeOf,
+                        seller: params.order.seller,
+                        customer: params.order.customer,
+                        confirmationNumber: params.order.confirmationNumber,
+                        orderNumber: params.order.orderNumber,
+                        price: params.order.price,
+                        priceCurrency: params.order.priceCurrency,
+                        orderDate: params.order.orderDate
+                    }
+                });
+            }
+        });
     } else {
         // ポイントインセンティブに対する承認アクションの分だけ、ポイントインセンティブ付与アクションを作成する
         const pointAwardAuthorizeActions =
