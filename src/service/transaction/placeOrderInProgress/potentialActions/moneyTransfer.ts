7e2533c29cb9f5c09caa42a9cec7677a8df1ccfe
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-04-30 07:55:56 +0000

    ### Changed - モジュールを細分化

---
+++ src/service/transaction/placeOrderInProgress/potentialActions/moneyTransfer.ts
@@ -0,0 +1,69 @@
+import * as factory from '../../../../factory';
+
+export type IAuthorizeMoneyTransferOffer = factory.action.authorize.offer.monetaryAmount.IAction<string>;
+
+export async function createMoneyTransferActions(params: {
+    order: factory.order.IOrder;
+    potentialActions?: factory.transaction.placeOrder.IPotentialActionsParams;
+    transaction: factory.transaction.placeOrder.ITransaction;
+}): Promise<factory.action.transfer.moneyTransfer.IAttributes<string>[]> {
+    const moneyTransferActions: factory.action.transfer.moneyTransfer.IAttributes<string>[] = [];
+
+    const authorizeMoneyTransferActions = (<IAuthorizeMoneyTransferOffer[]>params.transaction.object.authorizeActions)
+        .filter((a) => a.actionStatus === factory.actionStatusType.CompletedActionStatus)
+        .filter((a) => a.object.typeOf === 'Offer')
+        .filter((a) => a.object.itemOffered !== undefined && a.object.itemOffered.typeOf === 'MonetaryAmount');
+
+    const paymentMethod = params.order.paymentMethods[0];
+    authorizeMoneyTransferActions.forEach((a) => {
+        const actionResult = a.result;
+        const pendingTransaction = a.object.pendingTransaction;
+
+        if (actionResult !== undefined && pendingTransaction !== undefined) {
+            moneyTransferActions.push({
+                project: params.transaction.project,
+                typeOf: <factory.actionType.MoneyTransfer>factory.actionType.MoneyTransfer,
+                object: {
+                    pendingTransaction: actionResult.responseBody
+                },
+                agent: params.transaction.agent,
+                recipient: a.recipient,
+                amount: {
+                    typeOf: 'MonetaryAmount',
+                    value: Number(a.object.itemOffered.value),
+                    currency: pendingTransaction.object.toLocation.accountType
+                },
+                fromLocation: (paymentMethod !== undefined)
+                    ? {
+                        accountId: paymentMethod.accountId,
+                        typeOf: paymentMethod.typeOf,
+                        name: paymentMethod.name,
+                        paymentMethodId: paymentMethod.paymentMethodId,
+                        additionalProperty: paymentMethod.additionalProperty
+                    }
+                    : {
+                        typeOf: params.transaction.agent.typeOf,
+                        id: params.transaction.agent.id,
+                        name: <string>params.transaction.agent.name
+                    },
+                toLocation: pendingTransaction.object.toLocation,
+                purpose: {
+                    project: params.order.project,
+                    typeOf: params.order.typeOf,
+                    seller: params.order.seller,
+                    customer: params.order.customer,
+                    confirmationNumber: params.order.confirmationNumber,
+                    orderNumber: params.order.orderNumber,
+                    price: params.order.price,
+                    priceCurrency: params.order.priceCurrency,
+                    orderDate: params.order.orderDate
+                },
+                ...(typeof actionResult.responseBody.object.description === 'string')
+                    ? { description: actionResult.responseBody.object.description }
+                    : {}
+            });
+        }
+    });
+
+    return moneyTransferActions;
+}
