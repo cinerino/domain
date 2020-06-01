798691cb0b718f5db57bb14403d7ad64d286fd0a
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-06-01 22:39:05 +0000

    ### Changed - 返品取引を複数注文に対応

--- src/service/transaction/returnOrder.ts
+++ src/service/transaction/returnOrder.ts
@@ -47,20 +47,22 @@ export function start(
     }) => {
         const project = await repos.project.findById({ id: params.project.id });
         const seller = await repos.seller.findById({ id: params.seller.id });
-        const order = await repos.order.findByOrderNumber({ orderNumber: params.object.order.orderNumber });
 
-        const placeOrderTransactions = await repos.transaction.search<factory.transactionType.PlaceOrder>({
-            typeOf: factory.transactionType.PlaceOrder,
-            result: {
-                order: { orderNumbers: [params.object.order.orderNumber] }
-            }
-        });
-        const placeOrderTransaction = placeOrderTransactions.shift();
-        if (placeOrderTransaction === undefined) {
-            throw new factory.errors.NotFound('Transaction');
+        if (!Array.isArray(params.object.order)) {
+            params.object.order = [params.object.order];
         }
 
-        await validateOrder({ order })(repos);
+        const orders = await repos.order.search({
+            project: { id: { $eq: project.id } },
+            orderNumbers: params.object.order.map((o) => o.orderNumber),
+            seller: { ids: [seller.id] }
+        });
+
+        if (orders.length !== params.object.order.length) {
+            throw new factory.errors.NotFound('Order');
+        }
+
+        await validateOrder({ orders })(repos);
 
         checkReturnPolicy({
             reason: params.object.reason,
@@ -73,9 +75,10 @@ export function start(
         });
 
         const transactionObject: factory.transaction.returnOrder.IObject = {
-            order: order,
+            order: orders.map((o) => {
+                return { orderNumber: o.orderNumber };
+            }),
             cancellationFee: params.object.cancellationFee,
-            pendingCancelReservationTransactions: [],
             reason: params.object.reason,
             onOrderStatusChanged: {
                 informOrder: informOrderParams
@@ -117,39 +120,29 @@ export function start(
             throw error;
         }
 
-        // Chevre予約の場合、予約キャンセル取引開始する？
-        // いったん保留中
-        const pendingCancelReservationTransactions = await startCancelReservation({ placeOrderTransaction: placeOrderTransaction });
-        await repos.transaction.transactionModel.findByIdAndUpdate(
-            returnOrderTransaction.id,
-            { 'object.pendingCancelReservationTransactions': pendingCancelReservationTransactions }
-        )
-            .exec();
-
         return returnOrderTransaction;
     };
 }
 
 function validateOrder(params: {
-    order: factory.order.IOrder;
+    orders: factory.order.IOrder[];
 }) {
     return async (repos: {
         invoice: InvoiceRepo;
     }) => {
-        const order = params.order;
-
         // 注文ステータスが配送済の場合のみ受け付け
-        if (order.orderStatus !== factory.orderStatus.OrderDelivered) {
-            throw new factory.errors.Argument('Order Number', `Invalid Order Status: ${order.orderStatus}`);
+        const allOrdersDelivered = params.orders.every((o) => o.orderStatus === factory.orderStatus.OrderDelivered);
+        if (!allOrdersDelivered) {
+            throw new factory.errors.Argument('Order Number', 'Invalid Order Status');
         }
 
         // 決済がある場合、請求書の状態を検証
-        if (order.paymentMethods.length > 0) {
-            const invoices = await repos.invoice.search({ referencesOrder: { orderNumbers: [order.orderNumber] } });
-            const allPaymentCompleted = invoices.every((invoice) => invoice.paymentStatus === factory.paymentStatusType.PaymentComplete);
-            if (!allPaymentCompleted) {
-                throw new factory.errors.Argument('order.orderNumber', 'Payment not completed');
-            }
+        // if (order.paymentMethods.length > 0) {
+        // }
+        const invoices = await repos.invoice.search({ referencesOrder: { orderNumbers: params.orders.map((o) => o.orderNumber) } });
+        const allPaymentCompleted = invoices.every((invoice) => invoice.paymentStatus === factory.paymentStatusType.PaymentComplete);
+        if (!allPaymentCompleted) {
+            throw new factory.errors.Argument('Order Number', 'Payment not completed');
         }
     };
 }
@@ -197,67 +190,6 @@ function createInformOrderParams(
     return informOrderParams;
 }
 
-async function startCancelReservation(params: {
-    placeOrderTransaction: factory.transaction.ITransaction<factory.transactionType.PlaceOrder>;
-}): Promise<factory.chevre.transaction.cancelReservation.ITransaction[]> {
-    const pendingCancelReservationTransactions: factory.chevre.transaction.cancelReservation.ITransaction[] = [];
-    const authorizeSeatReservationActions = <factory.action.authorize.offer.seatReservation.IAction<WebAPIIdentifier>[]>
-        params.placeOrderTransaction.object.authorizeActions
-            .filter((a) => a.object.typeOf === factory.action.authorize.offer.seatReservation.ObjectType.SeatReservation)
-            .filter((a) => a.actionStatus === factory.actionStatusType.CompletedActionStatus);
-
-    for (const authorizeSeatReservationAction of authorizeSeatReservationActions) {
-        if (authorizeSeatReservationAction.result === undefined) {
-            throw new factory.errors.NotFound('Result of seat reservation authorize action');
-        }
-
-        let responseBody = authorizeSeatReservationAction.result.responseBody;
-
-        if (authorizeSeatReservationAction.instrument === undefined) {
-            authorizeSeatReservationAction.instrument = {
-                typeOf: 'WebAPI',
-                identifier: factory.service.webAPI.Identifier.Chevre
-            };
-        }
-
-        switch (authorizeSeatReservationAction.instrument.identifier) {
-            case factory.service.webAPI.Identifier.COA:
-                // tslint:disable-next-line:max-line-length
-                responseBody = <factory.action.authorize.offer.seatReservation.IResponseBody<factory.service.webAPI.Identifier.COA>>responseBody;
-
-                // no op
-
-                break;
-
-            default:
-                // tslint:disable-next-line:max-line-length
-                responseBody = <factory.action.authorize.offer.seatReservation.IResponseBody<factory.service.webAPI.Identifier.Chevre>>responseBody;
-
-            // 予約キャンセル取引開始は保留
-
-            // pendingCancelReservationTransactions.push(await repos.cancelReservationService.start({
-            //     typeOf: factory.chevre.transactionType.CancelReservation,
-            //     agent: {
-            //         typeOf: returnOrderTransaction.agent.typeOf,
-            //         id: returnOrderTransaction.agent.id,
-            //         name: order.customer.name
-            //     },
-            //     object: {
-            //         transaction: {
-            //             typeOf: responseBody.typeOf,
-            //             id: responseBody.id
-            //         }
-            //     },
-            //     expires: moment(params.expires)
-            //         .add(1, 'month')
-            //         .toDate() // 余裕を持って
-            // }));
-        }
-    }
-
-    return pendingCancelReservationTransactions;
-}
-
 /**
  * 取引確定
  */
@@ -282,9 +214,11 @@ export function confirm(params: factory.transaction.returnOrder.IConfirmParams) 
             throw new factory.errors.Forbidden('Transaction not yours');
         }
 
+        const orderNumbers: string[] = transaction.object.order.map((o) => o.orderNumber);
+
         const orders = await repos.order.search({
             project: { id: { $eq: transaction.project.id } },
-            orderNumbers: [transaction.object.order.orderNumber]
+            orderNumbers: orderNumbers
         });
 
         const result: factory.transaction.returnOrder.IResult = {};
