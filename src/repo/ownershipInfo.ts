4319f1ec80f7604f001a06a95f2018f71745a2a1
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-06-08 04:57:35 +0000

    ### Added - 所有権レポートサービスを追加

--- src/repo/ownershipInfo.ts
+++ src/repo/ownershipInfo.ts
@@ -1,4 +1,4 @@
-import { Connection, Model } from 'mongoose';
+import { Connection, Document, Model, QueryCursor } from 'mongoose';
 import * as uuid from 'uuid';
 
 import { modelName } from './mongoose/model/ownershipInfo';
@@ -253,5 +253,28 @@ export class MongoRepository {
         return query.setOptions({ maxTimeMS: 10000 })
             .exec()
             .then((docs) => docs.map((doc) => doc.toObject()));
+    }
+
+    public stream<T extends factory.ownershipInfo.IGoodType>(
+        params: factory.ownershipInfo.ISearchConditions<T>
+    ): QueryCursor<Document> {
+        const conditions = MongoRepository.CREATE_MONGO_CONDITIONS(params);
+        const query = this.ownershipInfoModel.find((conditions.length > 0) ? { $and: conditions } : {})
+            .select({ __v: 0, createdAt: 0, updatedAt: 0 });
+
+        // tslint:disable-next-line:no-single-line-block-comment
+        /* istanbul ignore else */
+        if (params.limit !== undefined && params.page !== undefined) {
+            query.limit(params.limit)
+                .skip(params.limit * (params.page - 1));
+        }
+
+        // tslint:disable-next-line:no-single-line-block-comment
+        /* istanbul ignore else */
+        if (params.sort !== undefined) {
+            query.sort(params.sort);
+        }
+
+        return query.cursor();
     }
 }
